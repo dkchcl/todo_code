4bde9e160a81d058075bccd47bf480b5d0d5c74f
dkchcl <dkc.hcl@gmail.com>
2025-07-21 22:30:35 +0530

    Update provider.tf

--- Infra/provider.tf
+++ Infra/provider.tf
@@ -5,6 +5,14 @@ terraform {
       version = "4.33.0"
     }
   }
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "dkc-rg-01"
+    storage_account_name  = "dkcstoragestate"
+    container_name        = "dkccontainer"
+    key                   = "terraform.tfstate"
+  }
+}
 }
 
 provider "azurerm" {
