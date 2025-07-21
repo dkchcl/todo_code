6bf25edd2b6537c32ac1c91358b1d32705d23a63
dkchcl <dkc.hcl@gmail.com>
2025-07-21 20:59:20 +0530

    code of vm

---
+++ Infra/variable.tf
@@ -0,0 +1,80 @@
+# variable "resource_group_name" {
+#   description = "rg name"
+#   type        = string
+# }
+
+# variable "location" {
+#   description = "location"
+#   type        = string
+# }
+
+# variable "virtual_network_name" {
+#   description = "virtual network"
+#   type        = string
+# }
+
+# variable "address_space" {
+#   description = "address space"
+#   type        = list(string)
+# }
+
+# variable "frontend_subnet" {
+#   description = "frontend subnet"
+#   type        = string
+# }
+
+# variable "address_prefixes" {
+#   description = "address prefixes"
+#   type        = list(string)
+# }
+
+# variable "nsg_name" {
+#   description = "nsg"
+#   type        = string
+# }
+
+# variable "public_ip" {
+#   description = "public ip"
+#   type        = string
+# }
+
+# variable "nic_name" {
+#   description = "nic"
+#   type        = string
+# }
+
+# variable "ip_config" {
+#   description = "ip config name"
+#   type        = string
+# }
+
+# variable "vm_name" {
+#   description = "vm name"
+#   type        = string
+# }
+
+# variable "vm_size" {
+#   description = "vm name"
+#   type        = string
+# }
+
+# variable "publisher" {}
+
+# variable "offer" {}
+
+# variable "sku" {}
+
+# variable "os_disk_name" {}
+
+# variable "caching" {}
+
+# variable "create_option" {}
+
+# variable "managed_disk_type" {}
+
+# variable "computer_name" {}
+
+# variable "admin_username" {}
+
+# variable "admin_password" {}
+
